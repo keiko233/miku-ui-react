a8f31c8b4a35d00d13f622074592c7fe7120e4db
keiko233 <i@elaina.moe>
2024-03-08 16:40:50 +0800

    feat: complete nav bar

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,13 +1,9 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import WorkSpace from "../components/WorkSpace";
-import {
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-} from "@mui/material";
+import { List, ListItemButton, ListItemText } from "@mui/material";
 import devices from "../assets/devices.json";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const defaultTheme = createTheme();
 
@@ -16,24 +12,38 @@ interface Props {
 }
 
 const App = ({ children }: Props) => {
+  const navigate = useNavigate();
+
+  const [selected, setSelected] = useState("Home");
+
+  const Item = ({ text, link }: { text: string; link: string }) => {
+    return (
+      <ListItemButton
+        selected={selected === text}
+        onClick={() => {
+          navigate(link);
+          setSelected(text);
+        }}
+      >
+        <ListItemText primary={text} />
+      </ListItemButton>
+    );
+  };
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <WorkSpace
         children={children}
         drawerContainer={
           <List>
-            <ListItem key={"home"} disablePadding>
-              <ListItemButton>
-                <ListItemText primary={"Back Home"} />
-              </ListItemButton>
-            </ListItem>
+            <Item text="Home" link="/" />
 
             {devices.map((item) => (
-              <ListItem key={item.codename} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={item.device} />
-                </ListItemButton>
-              </ListItem>
+              <Item
+                key={item.codename}
+                text={item.device}
+                link={`/devices/${item.codename}`}
+              />
             ))}
           </List>
         }
